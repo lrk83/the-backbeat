ba918231ce791e1f380ef310fbcdc11e3357c817
Lukas Keel <lrk83@cornell.edu>
2021-10-01 15:03:03 -0400

    user controller

--- server/controllers/user-controller.js
+++ server/controllers/user-controller.js
@@ -1,6 +1,66 @@
 const { User } = require('../models');
 const { signToken } = require('../utils/auth');
 
-const userController = {
+module.exports = {
+    async getSingleUser({ user = null, params }, res) {
+        const foundUser = await User.findOne({
+          $or: [{ _id: user ? user._id : params.id }, { username: params.username }],
+        });
     
+        if (!foundUser) {
+          return res.status(400).json({ message: 'Cannot find a user with this id!' });
+        }
+    
+        res.json(foundUser);
+      },
+      // create a user, sign a token, and send it back (to client/src/components/SignUpForm.js)
+      async createUser({ body }, res) {
+        const user = await User.create(body);
+    
+        if (!user) {
+          return res.status(400).json({ message: 'Something is wrong!' });
+        }
+        const token = signToken(user);
+        res.json({ token, user });
+      },
+      async login({ body }, res) {
+        const user = await User.findOne({ $or: [{ username: body.username }, { email: body.email }] });
+        if (!user) {
+          return res.status(400).json({ message: "Can't find this user" });
+        }
+    
+        const correctPw = await user.isCorrectPassword(body.password);
+    
+        if (!correctPw) {
+          return res.status(400).json({ message: 'Wrong password!' });
+        }
+        const token = signToken(user);
+        res.json({ token, user });
+      },
+      async savePost({ user, body }, res) {
+        
+        try {
+          const updatedUser = await User.findOneAndUpdate(
+            { _id: user._id },
+            { $addToSet: { savedPosts: body } },
+            { new: true, runValidators: true }
+          );
+          return res.json(updatedUser);
+        } catch (err) {
+          console.log(err);
+          return res.status(400).json(err);
+        }
+      },
+      // remove a post from `savedPosts`
+      async deletePost({ user, params }, res) {
+        const updatedUser = await User.findOneAndUpdate(
+          { _id: user._id },
+          { $pull: { savedPosts: { postId: params.postId } } },
+          { new: true }
+        );
+        if (!updatedUser) {
+          return res.status(404).json({ message: "Couldn't find user with this id!" });
+        }
+        return res.json(updatedUser);
+      },
 }
