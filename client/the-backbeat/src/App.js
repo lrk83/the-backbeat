859ab09c047c1086431ac82b83a0d3d1e45897ae
Lukas Keel <lrk83@cornell.edu>
2021-10-03 18:06:22 -0400

    set up apollo and router

--- client/the-backbeat/src/App.js
+++ client/the-backbeat/src/App.js
@@ -1,24 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { ApolloProvider } from '@apollo/client';
+import ApolloClient from 'apollo-boost';
+
+import Navbar from './components/Navbar';
+import HomePage from './pages/HomePage';
+
+const client = new ApolloClient({
+  request: operation => {
+    const token = localStorage.getItem('id_token');
+
+    operation.setContext({
+      headers: {
+        authorization: token ? `Bearer ${token}` : ''
+      }
+    });
+  },
+  uri: '/graphql'
+});
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ApolloProvider client={client}>
+      <Router>
+        <>
+          <Navbar />
+          <Switch>
+            <Route exact path="/" component={HomePage} />
+          </Switch>
+        </>
+      </Router>
+    </ApolloProvider>
   );
 }
 
---
+++ client/the-backbeat/src/components/Navbar.js
@@ -0,0 +1,11 @@
+import React, { useState } from 'react';
+
+const appNavbar = () => {
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default appNavbar;
