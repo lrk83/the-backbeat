2005439d1439f2af7087df712f1ab0e5de290678
Lukas Keel <lrk83@cornell.edu>
2021-10-10 22:22:40 -0400

    setting up sorted skills page

--- client/src/pages/SkillPage.js
+++ client/src/pages/SkillPage.js
@@ -1,8 +1,14 @@
 import React from 'react';
+import { Container } from 'semantic-ui-react';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+import { useQuery } from '@apollo/client';
+import {GET_SKILLS_FOR_SUGGESTED} from '../utils/queries';
 
 const SkillPage = () => {
     return (
-        <div></div>
+        <Container className="big-container" data-aos="fade-in" data-aos-delay="100" data-aos-duration="1500" >
+        </Container>
     )
 }
 
