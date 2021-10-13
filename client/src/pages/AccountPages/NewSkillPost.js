3db9eebeb0e5d0eaa6fbf336d1efebdf70fd988e
Lukas Keel <lrk83@cornell.edu>
2021-10-13 12:43:44 -0400

    updates new skill post form to include new link inputs

--- client/src/pages/AccountPages/NewSkillPost.js
+++ client/src/pages/AccountPages/NewSkillPost.js
@@ -16,7 +16,7 @@ const NewSkillPost = () => {
 
     const loggedIn = Auth.loggedIn();
 
-    const sections=["post-content","text","tags"]
+    const sections=["post-content","text","links","tags"]
 
     const [section, updateSection] = useState(sections[0]);
     const [contentData, setContentFormData] = useState({name:"",image:"",description:"",text:"",links:[]});
@@ -24,6 +24,10 @@ const NewSkillPost = () => {
     const [readyToSubmit, setReadyToSubmit] = useState(false);
     const [showAlert, setShowAlert] = useState(false);
     const [addTagForm, setAddTagForm] = useState('');
+    const [additionalLinkCount, setAddLinkCount] = useState([1]);
+    const [newLinkText, setNewLinkText] = useState("");
+
+    console.log(additionalLinkCount);
 
     const {loading, data} = useQuery(GET_TAGS);
     const tags = data?.tags || {};
@@ -36,13 +40,37 @@ const NewSkillPost = () => {
     //Handle which form sections are disabled
     const handleFirstFormSubmit = (event) => {
         event.preventDefault();
-        updateSection(sections[1]);
+        if (section==="post-content"){
+            updateSection(sections[1]);
+        }
+        if (section==="back-to-back"){
+            updateSection("back-to-text");
+        }
+        if (section==="back-to-basics"){
+            updateSection(sections[1]);
+        }
+        if (section==="all-the-way-back"){
+            updateSection("back-to-back-to-text");
+        }
     };
 
     const handleSecondFormSubmit = (event) => {
         event.preventDefault();
-        updateSection(sections[2]);
+        if (section==="text"){
+            updateSection(sections[2]);
+        }
+        if (section==="back-to-text"){
+            updateSection(sections[2])
+        }
+        if (section==="back-to-back-to-text"){
+            updateSection("back-to-links");
+        }
     };
+
+    const handleLinkFormSubmit = (event) => {
+        event.preventDefault();
+        updateSection(sections[3]);
+    }
 
     const handleThirdFormSubmit = (event) => {
         event.preventDefault();
@@ -92,6 +120,11 @@ const NewSkillPost = () => {
         setAddTagForm(value);
     }
 
+    const handleLinkContentChange = (event) => {
+        const {value} = event.target;
+        setNewLinkText(value);
+    }
+
     const handleAddTag = async (event) => {
         event.preventDefault();
 
@@ -111,18 +144,59 @@ const NewSkillPost = () => {
         setAddTagForm('');
     }
 
+    const handleAddLink = (event) => {
+        event.preventDefault();
+
+        let newAddLinkCount = []
+        for (let x=0;x<additionalLinkCount.length;x++){
+            newAddLinkCount.push(additionalLinkCount[x])
+        }
+
+        let nextCount = additionalLinkCount.length+1;
+        console.log(nextCount)
+        newAddLinkCount.push(nextCount);
+
+        console.log(newAddLinkCount)
+        setAddLinkCount(newAddLinkCount);
+
+        let newLinks = []
+        for(let x=0;x<contentData.links.length;x++){
+            newLinks.push(contentData.links[x]);
+        }
+
+        newLinks.push(newLinkText)
+
+        console.log(newLinks);
+
+        setContentFormData({...contentData, links: newLinks});
+        setNewLinkText("");
+    }
+
     const textBackButton = (event) => {
         event.preventDefault();
         if (section==="back-to-text"){
-            updateSection("back-to-back")
-        } else{
-        updateSection("back-to-basics")
-        };
+            updateSection("back-to-back");
+        }
+        if(section==="back-to-back-to-text"){
+            updateSection("all-the-way-back");
+        }
+        if(section==="text"){
+            updateSection("back-to-basics");
+        }
+    }
+
+    const linkBackButton = (event) => {
+        event.preventDefault();
+        if (section==="back-to-links"){
+            updateSection("back-to-back-to-text");
+        }else{
+            updateSection("back-to-text");
+        }
     }
 
     const tagsBackButton = (event) => {
         event.preventDefault();
-        updateSection("back-to-text");
+        updateSection("back-to-links");
     }
 
     const sendToBackEnd = async () => {
@@ -180,7 +254,7 @@ const NewSkillPost = () => {
                     {showAlert? (<Label basic color="red">Something went wrong. Please check your inputs and try again </Label>):(<></>)}
                         <Form.Group widths='equal' data-aos="fade-in" data-aos-delay="100" data-aos-duration="1500">
                             <Header as ="h2" className="new-post-header">Basic Info</Header>
-                            {section==="post-content" || section==="back-to-basics"  || section==="back-to-back" ? (<>
+                            {section==="post-content" || section==="back-to-basics"  || section==="back-to-back" || section==="all-the-way-back" ? (<>
                                 <Form.Input fluid label="Name" name="name" placeholder="Name" required onChange={handleContentChange}/>
                                 <Form.Input fluid label="Image" name="image" placeholder="Image URL (You can use right click - image adress)" required onChange={handleContentChange}/>
                                 <Form.Input fluid label = "Description" name="description" placeholder="Description" required onChange={handleContentChange}/>
@@ -202,12 +276,11 @@ const NewSkillPost = () => {
                             </>)}
                         </Form.Group>
                     </Form>
-                    {section==="text" || section==="tags" || section==="back-to-basics" || section==="back-to-text" || section==="back-to-back" ? (
+                    {section==="text" || section==="links" || section==="tags" || section==="back-to-basics" || section==="back-to-text" || section==="back-to-back" || section==="back-to-links" || section==="back-to-back-to-text" || section==="all-the-way-back" ? (
                          <Form onSubmit={handleSecondFormSubmit} data-aos="fade-in" data-aos-delay="100" data-aos-duration="1500">
                          <Header as ="h2" className="new-post-header">Text</Header>
-                         {showAlert? (<Label basic color="red">Something went wrong. Please check your inputs and try again </Label>):(<></>)}
                              <Form.Group widths='equal'>
-                                 {section==="text" || section==="back-to-text" ? (<>
+                                 {section==="text" || section==="back-to-text" || section==="back-to-back-to-text" ? (<>
                                      <Form.TextArea name="text" placeholder="post text" required onChange={handleContentChange}/>
                                      <Button
                                         onClick={textBackButton}>
@@ -237,10 +310,48 @@ const NewSkillPost = () => {
                              
                          </Form>
                     ) : (<></>)}
-                    {section==="tags" || section==="back-to-text" || section==="back-to-back" ? (
+                    {section==="links" || section==="tags" || section==="back-to-basics" || section==="back-to-text" || section==="back-to-back" || section==="back-to-links" || section==="back-to-back-to-text" || section==="all-the-way-back"? (
+                         <Form onSubmit={handleLinkFormSubmit} data-aos="fade-in" data-aos-delay="100" data-aos-duration="1500">
+                         <Header as ="h2" className="new-post-header">Links</Header>
+                             <Form.Group widths='equal'>
+                                 {section==="links" || section==="back-to-links" ? (<>
+                                     {additionalLinkCount.map(number=>(<>
+                                     <Form.Input fluid label="Add link" name="link" placeholder="Add link" onChange={handleLinkContentChange} key={number}/>
+                                     </>))}
+                                     <Button onClick={handleAddLink}>Add Link</Button>
+                                     <Button
+                                        onClick={linkBackButton}>
+                                        Back
+                                    </Button>
+                                    <Button
+                                        type='submit'
+                                        variant='success'>
+                                        Next
+                                    </Button>
+                                 </>):(<>
+                                     {additionalLinkCount.map(number=>(<>
+                                     <Form.Input fluid label="Add link" name="link" placeholder="Add link" disabled key={number}/>
+                                     </>))}
+                                     <Button disabled >Add Link</Button>
+                                     <Button
+                                        disabled>
+                                        Back
+                                    </Button>
+                                    <Button
+                                    disabled
+                                        type='submit'
+                                        variant='success'>
+                                        Next
+                                    </Button>
+                                 </>)}
+                                 <Form.Field></Form.Field>
+                             </Form.Group>
+                             
+                         </Form>
+                    ) : (<></>)}
+                    {section==="tags" || section==="back-to-back-to-text" || section==="all-the-way-back" || section==="back-to-links" ? (
                          <Form onSubmit={handleThirdFormSubmit} data-aos="fade-in" data-aos-delay="100" data-aos-duration="1500">
                          <Header as ="h2" className="new-post-header">Tags</Header>
-                         {showAlert? (<Label basic color="red">Something went wrong. Please check your inputs and try again </Label>):(<></>)}
                              {section==="tags" ? ( <>
                              <TagSearch tags={tags} chosenTags={chosenTags} setChosenTags={setChosenTags} contentData={contentData} setContentFormData={setContentFormData}/>
                              <Form.Group widths="equal">
@@ -299,6 +410,7 @@ const NewSkillPost = () => {
                          </Form>
                     ) : (<></>)}
                 </Container> ) }
+        {showAlert? (<Label basic color="red">Something went wrong. Please check your inputs and try again </Label>):(<></>)}
     </Container>
     )
 };
